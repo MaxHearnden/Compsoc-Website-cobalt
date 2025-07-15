ba92e64fcafe3983ca0c57f27cb10b5b687ec22a
MaxHearnden <maxoscarhearnden@gmail.com>
2025-07-15 04:36:13 +0100

    Improve formatting

--- package.nix
+++ package.nix
@@ -1,4 +1,4 @@
-{ cobalt, lib, nix-gitignore, simple-http-server, stdenv, tailwindcss_4 }:
+{ cobalt, lib, nix-gitignore, orbitron, poppins, simple-http-server, stdenv, tailwindcss_4 }:
 
 stdenv.mkDerivation {
   name = "compsoc-website";
@@ -24,6 +24,8 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir $out
     cp -r _site/img _site/index_final.css _site/index.html $out
+    cp -r ${orbitron}/share/fonts/opentype $out/orbitron
+    cp -r ${poppins}/share/fonts/truetype $out/Poppins
   '';
 
   nativeBuildInputs = [ cobalt tailwindcss_4 ];
