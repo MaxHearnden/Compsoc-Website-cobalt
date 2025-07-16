aa5e4dfdd291501686d6a18730f07fff302403b7
MaxHearnden <maxoscarhearnden@gmail.com>
2025-07-16 23:34:29 +0100

    Fix fonts

--- package.nix
+++ package.nix
@@ -1,4 +1,4 @@
-{ cobalt, html-minifier, lib, nix-gitignore, orbitron, poppins, simple-http-server, stdenv, tailwindcss_4, woff2 }:
+{ cobalt, html-minifier, lib, nix-gitignore, poppins, simple-http-server, stdenv, tailwindcss_4, woff2 }:
 
 stdenv.mkDerivation {
   name = "compsoc-website";
@@ -20,10 +20,11 @@ stdenv.mkDerivation {
       tailwindcss -m --cwd _site -i css/"$page".css -o "$page"_final.css
     done
 
-    cp ${orbitron}/share/fonts/opentype/Orbitron\ Light.otf _site/orbitron.otf
-    cp ${poppins}/share/fonts/truetype/Poppins-Light.ttf _site/poppins.ttf
-    woff2_compress _site/orbitron.otf
+    cp ${poppins}/share/fonts/truetype/Poppins-Light.ttf _site/poppins-light.ttf
+    cp ${poppins}/share/fonts/truetype/Poppins-Regular.ttf _site/poppins.ttf
+    woff2_compress _site/fonts/orbitron.ttf
     woff2_compress _site/poppins.ttf
+    woff2_compress _site/poppins-light.ttf
   '';
 
   installPhase = ''
@@ -34,7 +35,7 @@ stdenv.mkDerivation {
       html-minifier "''${minifierArgs[@]}" "_site/$page.html" -o "$out/$page.html"
     done
 
-    cp _site/orbitron.woff2 _site/poppins.woff2 $out
+    cp _site/fonts/orbitron.woff2 _site/poppins.woff2 _site/poppins-light.woff2 $out
   '';
 
   minifierArgs = [
